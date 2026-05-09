335bcec8a319a6b161ebf161253573c53d51a29f
Cioflingar <coder.walidhasan@gmail.com>
2026-05-09 11:38:54 +0600

    configure express

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,27 @@
-import express from 'express'
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
 
-const app = express()
+const app = express();
 
-export { app }
+app.use(
+  cors({
+    origin: precess.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
+app.use(
+  express.json({
+    limit: process.env.FILE_SIZE_LIMIT,
+  }),
+);
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: process.env.FILE_SIZE_LIMIT,
+  }),
+);
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export { app };
